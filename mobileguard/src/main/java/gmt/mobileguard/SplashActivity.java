f11713f1277b2df8e9aa467a788ef9602a285afc
Genment <tjm1991@foxmail.com>
2015-09-18 18:07:13 +0800

    Splash界面显示版本号

--- mobileguard/src/main/java/gmt/mobileguard/SplashActivity.java
+++ mobileguard/src/main/java/gmt/mobileguard/SplashActivity.java
@@ -1,13 +1,8 @@
 package gmt.mobileguard;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
+import android.widget.TextView;
 
 public class SplashActivity extends Activity {
 
@@ -15,6 +10,9 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //显示版本号
+        ((TextView) findViewById(R.id.version_name)).setText(getString(R.string.version_name, BuildConfig.VERSION_NAME));
     }
 
 }
