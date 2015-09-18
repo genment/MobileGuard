b1a66715954fba87ac9e2104a0da068d86be815e
Genment <tjm1991@foxmail.com>
2015-09-18 17:43:55 +0800

    第一次提交, Splash界面

---
+++ mobileguard/src/main/java/gmt/mobileguard/SplashActivity.java
@@ -0,0 +1,20 @@
+package gmt.mobileguard;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+}
