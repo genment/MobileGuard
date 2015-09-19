9dcc92a9f0d4aa964bbcc26e8ad3766c664da63d
Genment <tjm1991@foxmail.com>
2015-09-20 03:11:08 +0800

    添加下载更新apk的service, 以及在下载前检测是否有足够的可用空间

--- mobileguard/src/main/java/gmt/mobileguard/activity/HomeActivity.java
+++ mobileguard/src/main/java/gmt/mobileguard/activity/HomeActivity.java
@@ -3,10 +3,13 @@ package gmt.mobileguard.activity;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.widget.Toast;
 
 import gmt.mobileguard.R;
+import gmt.mobileguard.sevice.UpdateService;
 
 public class HomeActivity extends Activity {
 
@@ -18,7 +21,7 @@ public class HomeActivity extends Activity {
     }
 
     private void getNewVersion() {
-        Bundle updateInfo = getIntent().getExtras();
+        final Bundle updateInfo = getIntent().getExtras();
         if (updateInfo.getBoolean("hasNewVersion")) { // 如果有新版本
             new AlertDialog.Builder(this)
                     .setTitle("发现新版本: " + updateInfo.getString("versionName"))
@@ -26,7 +29,12 @@ public class HomeActivity extends Activity {
                     .setPositiveButton("立即更新", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
-                            Toast.makeText(getApplicationContext(), "正在下载新版本", Toast.LENGTH_SHORT).show();
+                            if (checkExternalStorageFreeSize()) { // 检测是否有足够的可用空间
+                                startDownloadService(updateInfo.getString("apk"));
+                                Toast.makeText(getApplicationContext(), "正在下载新版本", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "SD卡可用空间少于10MB, 请清理后再重试", Toast.LENGTH_SHORT).show();
+                            }
                         }
                     })
                     .setNegativeButton("以后再说", new DialogInterface.OnClickListener() {
@@ -40,5 +48,13 @@ public class HomeActivity extends Activity {
         }
     }
 
+    private void startDownloadService(String url) {
+        Intent intent = new Intent(this, UpdateService.class);
+        intent.putExtra("apk", url);
+        startService(intent);
+    }
 
+    private boolean checkExternalStorageFreeSize() {
+        return Environment.getExternalStorageDirectory().getUsableSpace() / 1024 / 1024 >= 10;
+    }
 }
---
+++ mobileguard/src/main/java/gmt/mobileguard/sevice/UpdateService.java
@@ -0,0 +1,98 @@
+package gmt.mobileguard.sevice;
+
+import android.app.DownloadManager;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+import gmt.mobileguard.R;
+
+/**
+ * Project: StudyDemo
+ * Package: gmt.mobileguard.utils
+ * Created by Genment at 2015/9/19 02:05.
+ */
+public class UpdateService extends Service {
+    DownloadManager.Request request;
+    DownloadManager manager;
+    long fileId;
+    DownloadReceiver downloadReceiver;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        System.out.println("onCreate");
+        manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        String url = intent.getStringExtra("apk");
+        request = new DownloadManager.Request(Uri.parse(url))
+                .setTitle(getString(R.string.app_name))
+                .setDescription("正在下载更新包...")
+                .setDestinationInExternalFilesDir(this, Environment.DIRECTORY_DOWNLOADS, url.substring(url.lastIndexOf('/') + 1))
+                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED) // visible and shows while in progress and after completion.
+                /*.setVisibleInDownloadsUi(true)*/; // Set whether this download should be displayed in the system's Downloads UI. true by default,
+        startListenDownload();
+        startDownload();
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        stopListenDownload();
+    }
+
+    public void startDownload() {
+        fileId = manager.enqueue(request);
+    }
+
+    public void cancelDownload() {
+        manager.remove(fileId);
+    }
+
+    private void startListenDownload() {
+        downloadReceiver = new DownloadReceiver();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
+        intentFilter.addAction(DownloadManager.ACTION_NOTIFICATION_CLICKED);
+        intentFilter.addAction(DownloadManager.ACTION_VIEW_DOWNLOADS);
+        this.registerReceiver(downloadReceiver, intentFilter);
+    }
+
+    private void stopListenDownload() {
+        if (downloadReceiver != null) {
+            this.unregisterReceiver(downloadReceiver);
+        }
+    }
+
+    class DownloadReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            switch (action) {
+                case DownloadManager.ACTION_DOWNLOAD_COMPLETE:
+                    stopService(new Intent(UpdateService.this, UpdateService.class)); // 下载完成后, 停止服务
+                    Toast.makeText(getApplicationContext(), "下载完成", Toast.LENGTH_SHORT).show();
+                    break;
+                case DownloadManager.ACTION_NOTIFICATION_CLICKED:
+                    // TODO: 2015/9/19 下载时点击通知后执行的事情
+                    break;
+            }
+        }
+    }
+}
