b2a61909cc103539e2447f65b4785a7650d08e21
Genment <tjm1991@foxmail.com>
2015-11-12 10:53:33 +0800

    添加 App 类（Application）

---
+++ mobileguard/src/main/java/gmt/mobileguard/App.java
@@ -0,0 +1,15 @@
+package gmt.mobileguard;
+
+import android.app.Application;
+
+/**
+ * Project: StudyDemo
+ * Package: gmt.mobileguard
+ * Created by Genment at 2015/11/7 16:20.
+ */
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
