559c58c998bfbe6fbb247f592f830136e352d12b
Genment <tjm1991@foxmail.com>
2015-11-14 18:18:31 +0800

    OK! SecurityGuide3.

--- mobileguard/src/main/java/gmt/mobileguard/fragment/SecurityGuide3.java
+++ mobileguard/src/main/java/gmt/mobileguard/fragment/SecurityGuide3.java
@@ -2,15 +2,21 @@ package gmt.mobileguard.fragment;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import gmt.mobileguard.BuildConfig;
 import gmt.mobileguard.R;
+import gmt.mobileguard.util.SharedPrefsCtrl;
 
 public class SecurityGuide3 extends Fragment implements View.OnClickListener {
     // 表示是否已经绑定 SIM 卡的 key
@@ -20,6 +26,9 @@ public class SecurityGuide3 extends Fragment implements View.OnClickListener {
     private String mPhoneNumber;
 
     private OnStepButtonClickedListener mListener;
+    private final int REQUEST_CODE_CONTACT = 1;
+
+    private TextView phone;
 
     /**
      * Use this factory method to create a new instance of
@@ -43,6 +52,11 @@ public class SecurityGuide3 extends Fragment implements View.OnClickListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 获取是否绑定SIM卡
+        mPhoneNumber = SharedPrefsCtrl.getString(SharedPrefsCtrl.Constant.SJFD_SECURITY_PHONE, "");
+
+        // TODO: 2015/11/14 暂时没用，以后再说。
         if (getArguments() != null) {
             mPhoneNumber = getArguments().getString(ARG_PHONE_NUMBER, "");
         }
@@ -56,19 +70,20 @@ public class SecurityGuide3 extends Fragment implements View.OnClickListener {
         contentView.findViewById(R.id.prev_step).setOnClickListener(this);
         contentView.findViewById(R.id.next_step).setOnClickListener(this);
 
-        TextView phone = (TextView) contentView.findViewById(R.id.guide_3_phone);
+        phone = (TextView) contentView.findViewById(R.id.guide_3_phone);
         phone.setText(mPhoneNumber);
-        Button select_phone = ((Button) contentView.findViewById(R.id.guide_3_select_phone));
-        select_phone.setOnClickListener(this);
+        contentView.findViewById(R.id.guide_3_select_phone).setOnClickListener(this);
         return contentView;
     }
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.guide_3_select_phone) {
-            // TODO: 2015/11/5 跳转到系统的获取联系人界面
-            Toast.makeText(getActivity(), R.string.tip_guide3_3, Toast.LENGTH_SHORT).show();
+            // 跳转到系统的获取联系人界面
+            openSystemContactsActivity();
         } else if (mListener != null) {
+            // 点击“上一步”/“下一步”时，保存号码
+            SharedPrefsCtrl.putString(SharedPrefsCtrl.Constant.SJFD_SECURITY_PHONE, phone.getText().toString());
             if (v.getId() == R.id.next_step) {
                 mListener.onStepButtonClicked(4);
             } else {
@@ -92,6 +107,40 @@ public class SecurityGuide3 extends Fragment implements View.OnClickListener {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    private void openSystemContactsActivity() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setData(ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        startActivityForResult(intent, REQUEST_CODE_CONTACT);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data != null && requestCode == REQUEST_CODE_CONTACT) {
+            phone.setText(getSecurityPhone(data.getDataString()));
+        }
+    }
+
+    public String getSecurityPhone(String uri) {
+        if (BuildConfig.DEBUG) Log.d("TEST", uri);
+        ContentResolver resolver = getActivity().getContentResolver();
+        Cursor cursor = resolver.query(
+                Uri.parse(uri),
+                new String[]{ // 查找的列
+                        ContactsContract.CommonDataKinds.Phone.NUMBER // 号码
+                },
+                null, // 条件
+                null, // 条件的参数
+                null  // 排序
+        );
+        if (cursor != null && cursor.moveToFirst()) {
+            String number = cursor.getString(0);
+            cursor.close();
+            return number;
+        }
+        return "";
     }
 
     /**
